bacb5866613a72007470b96378bcfa25a4fbebbf
JRSiwiecki <JRSiwiecki@gmail.com>
2023-05-06 23:49:01 -0700

    Update driver

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -1,21 +1,12 @@
 package main;
 
-import api.*;
+import gui.MainGUI;
 
 public class Driver
 {
 	public static void main(String[] args)
 	{
-//		StudentDAO.createStudent("014851590", "Abdiel", "CS");
-//		StudentDAO.createStudent("014456741", "Jonathan", "CS");
-		
-//		AuthorDAO.createAuthor("George Orwell", "British", "Politics");
-//		AuthorDAO.createAuthor("Virginia Woolf", "British", "Stream of Consciousness");
-		
-//		DirectorDAO.createDirector("Spike Lee", "American", "Politics");
-//		DirectorDAO.createDirector("James Cameron", "Canadian", "Action");
-		
-//		BookDAO.createBook("", null, null, 0, 0, null, null, null);
-//		BookDAO.createBook("", null, null, 0, 0, null, null, null);
+		MainGUI mainGUI = new MainGUI();
+		mainGUI.showWindow();
 	}
 }
